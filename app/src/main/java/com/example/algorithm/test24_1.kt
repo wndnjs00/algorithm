f67d27a9e24984a9b30e3ba6847517e5c7ce5397
jeonjuwon <juwonjeon852@gmail.com>
2024-03-26 13:56:30 +0900

    나누어 떨어지는 숫자

---
+++ app/src/main/java/com/example/algorithm/test24_1.kt
@@ -0,0 +1,17 @@
+//package com.example.algorithm
+//
+//
+//fun main() {
+//
+//    fun solution(seoul: Array<String>): String {
+//        var answer = ""
+//
+//        // indexOf("Kim")은 Kim의 인덱스를 호출!
+//        answer = "김서방은 ${seoul.indexOf("Kim")}에 있다"
+//
+//        return answer
+//    }
+//
+//
+//    println(solution(seoul = arrayOf("Jang","youn","Kim")))
+//}
