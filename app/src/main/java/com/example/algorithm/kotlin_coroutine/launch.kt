b8bf3b175b701b71e4f9d2d5da921fa9d58bd404
wndnjs00 <juwonjeon852@gmail.com>
2024-09-07 17:12:30 +0900

    launch.kt 수정

--- app/src/main/java/com/example/algorithm/kotlin_coroutine/launch.kt
+++ app/src/main/java/com/example/algorithm/kotlin_coroutine/launch.kt
@@ -1,5 +1,7 @@
 package com.example.algorithm.kotlin_coroutine
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -37,33 +39,33 @@ fun main(){
 // 아무것도 출력X
 // IOThread는 MainThread가 종료되면 같이 종료되기 때문!
 // 해결하기 위해서는 IOThread 작업이 완료될 때까지 MainThread에서 기다려줘야 함
-//fun main() {
-//    val job = CoroutineScope(Dispatchers.IO).launch() {
-//        delay(3000)
-//        println("launch완료")
-//    }
-//}
+fun main000() {
+    val job = CoroutineScope(Dispatchers.IO).launch() {
+        delay(3000)
+        println("launch완료")
+    }
+}
 
 
 // MainThread에서 delay(중단기능)를 사용하기위해 suspend 키워드를 붙이고,
 // delay로 MainThread를 Blocking 시켜 4초간 기다리게 만들었음
 // 그치만 우리는 IO Thread가 언제 끝날지 항상 예측할 수 없기때문에, delay()로 하는 건 좋은방법X
-//suspend fun main() {
-//    val job = CoroutineScope(Dispatchers.IO).launch() {
-//        delay(3000)
-//        println("launch완료")
-//    }
-//    delay(4000)
-//}
+suspend fun main011() {
+    val job = CoroutineScope(Dispatchers.IO).launch() {
+        delay(3000)
+        println("launch완료")
+    }
+    delay(4000)
+}
 
 
 // launch는 job객체 반환
 // job.join() 메서드를 사용하면 job이 종료될때까지 실행되고 있는 코루틴을 일시중단 시킴
 // 이렇게하면 얼마나 걸릴지 모르는 작업도 수행가능
-//suspend fun main() {
-//    val job = CoroutineScope(Dispatchers.IO).launch() {
-//        delay(3000)
-//        println("launch완료")
-//    }
-//    job.join()
-//}
+suspend fun main012() {
+    val job = CoroutineScope(Dispatchers.IO).launch() {
+        delay(3000)
+        println("launch완료")
+    }
+    job.join()
+}
